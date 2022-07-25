9d07a598613f74035407f64909f538a18def422b
popcorn <popcorn9499@gmail.com>
2022-07-25 01:00:57 -0500

    made this output a ListTile

--- lib/BusListTile.dart
+++ lib/BusListTile.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import "package:transit_app/BusStatus.dart";
 
-
 class BusListItemData extends StatelessWidget {
-  BusListItemData ({ Key? key, required this.timeRemaining, required this.busStatus, required this.stopName}) : super(key: key);
+  BusListItemData(
+      {Key? key,
+      required this.timeRemaining,
+      required this.busStatus,
+      required this.stopName})
+      : super(key: key);
   String timeRemaining;
   BusStatus busStatus;
   String stopName;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return ListTile(
+      title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Icon(Icons.map),
@@ -20,13 +25,13 @@ class BusListItemData extends StatelessWidget {
           Expanded(
             flex: 4,
             child: Text(busStatus.toShortString(), textAlign: TextAlign.right),
-
           ),
           Expanded(
             flex: 1,
             child: Text(timeRemaining, textAlign: TextAlign.right),
           ),
-        ]
+        ],
+      ),
     );
   }
 }
