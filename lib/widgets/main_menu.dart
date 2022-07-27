0c686ed8d49174f3a68c8a876e91ca0d94355110
popcorn <popcorn9499@gmail.com>
2022-07-26 22:15:32 -0500

    fix the test functions

--- lib/widgets/main_menu.dart
+++ lib/widgets/main_menu.dart
@@ -123,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.access_time),
+        child: const Icon(Icons.add),
       ),
 
       // This trailing comma makes auto-formatting nicer for build methods.
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:untitled/main.dart';
+import 'package:transit_app/widgets/main_menu.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MainMenu());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('0 times'), findsOneWidget);
+    expect(find.text('1 times'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('0 times'), findsNothing);
+    expect(find.text('1 times'), findsOneWidget);
   });
 }
