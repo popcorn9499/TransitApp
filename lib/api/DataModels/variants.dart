caed78da2bc05ed05de54a9ee0cd48ffef35d286
popcorn9499 <popcorn9499@gmail.com>
2022-08-15 17:10:32 -0500

    cleanup variants obj

--- lib/api/DataModels/variants.dart
+++ lib/api/DataModels/variants.dart
@@ -5,13 +5,23 @@ class Variants {
   Variants({required this.name, required this.variants});
   final String name;
   final List<Variant> variants;
-  
+
+  //preform a shallow copy of the list to ensure the list structure is immutable
+  List<Variant> getVariants() {
+    List<Variant> result = [];
+    for (Variant element in variants){
+      result.add(element);
+    }
+    return result;
+  }
+
   @override
   String toString() {
     String result = "$name: $variants";
     return result;
   }
 
+  //take the json input map and return a constructed Variants obj
   factory Variants.fromJson(String name, Map<String, dynamic> data) {
     List<dynamic> variantsData = data["variants"];
     List<Variant> variants = [];
