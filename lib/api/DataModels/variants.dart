9cf194e9c81d433dcbcecb548630f9278418cc03
popcorn9499 <popcorn9499@gmail.com>
2022-08-15 17:00:20 -0500

    improvements to variants data obj

--- lib/api/DataModels/variants.dart
+++ lib/api/DataModels/variants.dart
@@ -1,10 +1,27 @@
+import 'variant.dart';
+
+
 class Variants {
   Variants({required this.name, required this.variants});
   final String name;
-  final Map<String, String> variants;
+  final List<Variant> variants;
+  
+  @override
+  String toString() {
+    String result = "$name: $variants";
+    return result;
+  }
 
   factory Variants.fromJson(String name, Map<String, dynamic> data) {
-    final variants = data as Map<String, String>;
+    List<dynamic> variantsData = data["variants"];
+    List<Variant> variants = [];
+    for (var element in variantsData) {
+      String elemKey = element["key"] as String;
+      String elemName = element["name"] as String;
+      Variant variant = Variant(key: elemKey, name: elemName);
+      variants.add(variant);
+    }
+
     return Variants(name: name, variants: variants);
   }
 }
