a939b3e812b2ac5d051abe430d496538c43f36ab
popcorn <popcorn9499@gmail.com>
2022-08-26 17:30:59 -0500

    data obj for storing bus arrival and departure times

---
+++ lib/api/DataModels/bus_info.dart
@@ -0,0 +1,25 @@
+import "package:transit_app/api/DataModels/bus_stop.dart";
+import "package:transit_app/api/DataModels/variant.dart";
+
+class BusInfo {
+
+  BusInfo({required this.stop, required this.number, required this.name, required this.arrivalScheduled,
+    required this.arrivalEstimated, required this.departureEstimated, required this.departureScheduled, required this.variant});
+
+  final BusStop stop;
+  final int number;
+  final String name;
+  final String arrivalScheduled;
+  final String arrivalEstimated;
+  final String departureScheduled;
+  final String departureEstimated;
+  final Variant variant;
+
+  @override
+  String toString() {
+    String result = "";
+    
+
+    return result;
+  }
+}
