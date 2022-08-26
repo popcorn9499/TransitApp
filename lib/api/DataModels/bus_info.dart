b4a2ed2694b2847411a2f29c1c5aa638b5de8ad5
popcorn <popcorn9499@gmail.com>
2022-08-26 18:11:31 -0500

    some code cleanup based on existing data objects

--- lib/api/DataModels/bus_info.dart
+++ lib/api/DataModels/bus_info.dart
@@ -1,14 +1,14 @@
 import "package:transit_app/api/DataModels/bus_stop.dart";
 import "package:transit_app/api/DataModels/variant.dart";
+import "package:transit_app/api/DataModels/route.dart";
 
 class BusInfo {
 
-  BusInfo({required this.stop, required this.routeNumber, required this.name, required this.arrivalScheduled,
+  BusInfo({required this.stop, required this.route, required this.arrivalScheduled,
     required this.arrivalEstimated, required this.departureEstimated, required this.departureScheduled, required this.variant});
 
   final BusStop stop;
-  final String routeNumber;
-  final String name;
+  final Route route;
   final String arrivalScheduled;
   final String arrivalEstimated;
   final String departureScheduled;
@@ -17,7 +17,8 @@ class BusInfo {
 
   @override
   String toString() {
-    String result = "Bus #$routeNumber $name arrival scheduled: $arrivalScheduled arrival estimated: $arrivalEstimated ";
+    String result = "Bus $route";
+    result += "arrival scheduled: $arrivalScheduled arrival estimated: $arrivalEstimated ";
     result += "departure scheduled $departureScheduled departure estimated $departureEstimated ";
     result += variant.toString();
     result += " at stop ";
@@ -26,12 +27,13 @@ class BusInfo {
   }
 
   //take the json input map and return a constructed Variants obj
-  factory BusInfo.fromJson(String name, Map<String, dynamic> data) {
-    Map<String, dynamic> stopData = data["stop"];
+  //stop data being a map<string, dynamic> storing the "stop" keyword from a given json string. this includes key, name, number, direction etc
+  //routeInfo being map<string, dynamic> this stores both "route" and "scheduled-stops"
+  //modeled after https://api.winnipegtransit.com/v3/stops/10611/schedule.json?usage=short&api-key={apikey}&start=2022-08-26T16:20:00
+  factory BusInfo.fromJson(String name, Map<String, dynamic> stopData, Map<String, dynamic> routeInfo) {
     BusStop stop = BusStop.fromJson(stopData);
-    final name = data["name"] as String;
-    final routeNumber = data["number"] as String;
-    final
+    final Route route = Route.fromJson(routeInfo["route"]);
+    final 
 
 
     return Variants(name: name, variants: variants);
