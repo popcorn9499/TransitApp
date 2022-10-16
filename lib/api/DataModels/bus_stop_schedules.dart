10f94e9be824277515bbf86fc0e3ca5189fb98a8
popcorn <popcorn9499@gmail.com>
2022-10-15 22:32:21 -0500

    decided against extending the BusStop class

--- lib/api/DataModels/bus_stop_schedules.dart
+++ lib/api/DataModels/bus_stop_schedules.dart
@@ -1,4 +1,12 @@
 import "package:transit_app/api/DataModels/bus_stop.dart";
-class BusStopSchedules extends BusStop {
+import 'package:transit_app/api/DataModels/bus_info.dart';
+
+
+class BusStopSchedules {
+  BusStopSchedules({required this.schedules, required this.busStop});
+  final BusStop busStop;
+  final List<BusInfo> schedules;
+
+
 
 }
