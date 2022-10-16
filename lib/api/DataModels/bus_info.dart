e93fdf51fd0934ac10af0da025e98904adb1d9e4
popcorn <popcorn9499@gmail.com>
2022-10-15 22:13:40 -0500

    cleaned up bus info
    this should allow us to send a bunch of parts of our map to create a list of bus infos in our schedule

--- lib/api/DataModels/bus_info.dart
+++ lib/api/DataModels/bus_info.dart
@@ -33,21 +33,20 @@ class BusInfo {
 
   //take the json input map and return a constructed Variants obj
   //stop data being a map<string, dynamic> storing the "stop" keyword from a given json string. this includes key, name, number, direction etc
-  //routeInfo being map<string, dynamic> this stores both "route" and "scheduled-stops"
+  //routeInfo being map<string, dynamic> this "scheduled-stops"[i] some specific number of the stops
   //modeled after https://api.winnipegtransit.com/v3/stops/10611/schedule.json?usage=short&api-key={apikey}&start=2022-08-26T16:20:00
-  factory BusInfo.fromJson(String name, BusStop stop, Map<String, dynamic> routeInfo) {
-    final Route route = Route.fromJson(routeInfo["route"]);
-    final busNumber = routeInfo["scheduled-stops"][0]["bus"]["key"] as int;
-    final bikeRack = routeInfo["scheduled-stops"][0]["bus"]["bike-rack"] as bool;
-    final wifi = routeInfo["scheduled-stops"][0]["bus"]["wifi"] as bool;
-    final cancelled = routeInfo["scheduled-stops"][0]["bus"]["cancelled"] as bool;
-    final variantKey = routeInfo["scheduled-stops"][0]["bus"]["variant"]["key"] as String;
-    final variantName = routeInfo["scheduled-stops"][0]["bus"]["variant"]["name"] as String;
+  factory BusInfo.fromJson(String name, BusStop stop, Route route, Map<String, dynamic> routeInfo) {
+    final busNumber = routeInfo["bus"]["key"] as int;
+    final bikeRack = routeInfo["bus"]["bike-rack"] as bool;
+    final wifi = routeInfo["bus"]["wifi"] as bool;
+    final cancelled = routeInfo["bus"]["cancelled"] as bool;
+    final variantKey = routeInfo["bus"]["variant"]["key"] as String;
+    final variantName = routeInfo["bus"]["variant"]["name"] as String;
     final Variant variant = Variant(name: variantName, key: variantKey);
-    final arrivalScheduled = routeInfo["scheduled-stops"][0]["times"]["arrival"]["scheduled"] as String;
-    final arrivalEstimated = routeInfo["scheduled-stops"][0]["times"]["arrival"]["estimated"] as String;
-    final departureScheduled = routeInfo["scheduled-stops"][0]["times"]["departure"]["scheduled"] as String;
-    final departureEstimated = routeInfo["scheduled-stops"][0]["times"]["departure"]["estimated"] as String;
+    final arrivalScheduled = routeInfo["times"]["arrival"]["scheduled"] as String;
+    final arrivalEstimated = routeInfo["times"]["arrival"]["estimated"] as String;
+    final departureScheduled = routeInfo["times"]["departure"]["scheduled"] as String;
+    final departureEstimated = routeInfo["times"]["departure"]["estimated"] as String;
 
     return BusInfo(stop: stop, route: route, arrivalScheduled: arrivalScheduled, arrivalEstimated: arrivalEstimated, departureEstimated: departureEstimated, departureScheduled: departureScheduled, variant: variant, busNumber: busNumber, bikeRack: bikeRack, wifi: wifi, cancelled: cancelled);
   }
