3ad370bd0ebb56bd1411a0b92ef9bda07dfdb848
popcorn <popcorn9499@gmail.com>
2022-10-15 23:36:08 -0500

    filled out the handling importing from json

--- lib/api/DataModels/bus_stop_schedules.dart
+++ lib/api/DataModels/bus_stop_schedules.dart
@@ -1,6 +1,6 @@
 import "package:transit_app/api/DataModels/bus_stop.dart";
 import 'package:transit_app/api/DataModels/bus_info.dart';
-
+import 'package:transit_app/api/DataModels/route.dart';
 
 class BusStopSchedules {
   BusStopSchedules({required this.schedules, required this.busStop});
@@ -8,5 +8,20 @@ class BusStopSchedules {
   final List<BusInfo> schedules;
 
 
+  factory BusStopSchedules.fromJson(Map<String, dynamic> data) {
+    Route route;
+    List<BusInfo> busSchedules = [];
+    BusStop busStop = BusStop.fromJson(data["stop"]);
 
+    //loop over every route to find all the routes at a stop
+    for (var routeSchedule in data["route-schedules"]){
+      route = Route.fromJson(routeSchedule["route"]);
+      //loop over every bus in each route at a stop
+      for (var busSchedule in routeSchedule["scheduled-stops"]){
+        busSchedules.add(BusInfo.fromJson(busStop, route, busSchedule));
+      }
+    }
+
+    return BusStopSchedules(schedules: busSchedules, busStop: busStop);
+  }
 }
