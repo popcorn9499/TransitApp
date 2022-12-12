40f69b6caeab25fe69a18b613762c8f4d1cd4e28
popcorn <popcorn9499@gmail.com>
2022-12-12 16:41:45 -0600

    some basic tests for bus_stop

--- test/api/DataModels/bus_stop_test.dart
+++ test/api/DataModels/bus_stop_test.dart
@@ -6,10 +6,23 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:transit_app/api/DataModels/bus_stop.dart';
 import 'package:transit_app/api/DataModels/variant.dart';
 
 
 void main() {
+  test('Test bus Stop Creation', () async {
+    BusStop busStop = BusStop(key: 1234, number: 1234, name: "cool stop", direction: "South");
 
+    expect(busStop.key, equals(1234));
+    expect(busStop.number, equals(1234));
+    expect(busStop.name, equals("cool stop"));
+    expect(busStop.direction, equals("South"));
+  });
+
+  test('Test bus Stop toString', () async {
+    BusStop busStop = BusStop(key: 1234, number: 1234, name: "cool stop", direction: "South");
+    expect(busStop.toString(), equals("Stop #1234 at cool stop direction South"));
+  });
 }
 
