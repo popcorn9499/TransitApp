c7884f52f1a86ed82ac12cb2f6a8dd6e2ad1ba19
popcorn <popcorn9499@gmail.com>
2022-12-23 19:36:16 -0600

    url generator improvements

--- lib/api/URLGenerator.dart
+++ lib/api/URLGenerator.dart
@@ -3,11 +3,16 @@ class URLGenerator {
   URLGenerator({required this.url});
 
   void addParam(String key, String value) {
-    url += "$key&$value";
+    url += "&$key=$value";
+  }
+
+  void addParamList(String key, List<String> values) {
+
   }
 
   @override
   String toString() {
-    return url;
+    var regex = RegExp('\\s'); //handle replacing spaces with +
+    return url.replaceAll(regex, "+");
   }
 }
