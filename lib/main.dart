40cdcb96425683d380e1ef9b43739aa8b8eb276f
popcorn <popcorn9499@gmail.com>
2023-01-03 01:12:14 -0600

    a

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'package:transit_app/widgets/menus/main_menu.dart';
 import 'Config/favorite_manager.dart';
 
 void main() {
-  FavoriteManager fm = FavoriteManager();
-  runApp(MainMenu(fm: fm));
+  runApp(MainMenu());
 
 }
 
