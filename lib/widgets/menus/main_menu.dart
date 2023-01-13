7c83003d658e5c7d48fe37631faf205a5cef65a3
popcorn <popcorn9499@gmail.com>
2023-01-12 22:44:04 -0600

    word towards a dark mode

--- lib/widgets/menus/main_menu.dart
+++ lib/widgets/menus/main_menu.dart
@@ -10,6 +10,7 @@ import 'package:transit_app/widgets/widgets/error_snackbar.dart';
 import '../../Config/favorite_manager.dart';
 import '../../api/DataModels/bus_stop.dart';
 import '../../api/Exceptions/NetworkError.dart';
+import '../styles/Style.dart';
 import 'close_stops_menu.dart';
 import 'favorites_menu.dart';
 
@@ -23,18 +24,7 @@ class MainMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Transit App',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepPurple,
-      ),
+      theme: Styles.themeData(true, context),
       home: MyHomePage(title: 'Transit App'),
     );
   }
