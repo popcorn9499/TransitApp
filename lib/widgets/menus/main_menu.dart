30820ba5275aad4ee03aea3142afdf698196fd62
popcorn <popcorn9499@gmail.com>
2023-01-18 22:07:56 -0600

    cleanup

--- lib/widgets/menus/main_menu.dart
+++ lib/widgets/menus/main_menu.dart
@@ -97,10 +97,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   }
 
-  void bringUpPopup() {
-    ;
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
