1e9b5e6f3dc9bc9a67cf4a2a5ff5d7444f91b125
popcorn <popcorn9499@gmail.com>
2023-01-18 21:49:18 -0600

    cleaned out dead code

--- lib/widgets/menus/main_menu.dart
+++ lib/widgets/menus/main_menu.dart
@@ -63,22 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
     _controller = TextEditingController();
     errorPrompt = ErrorSnackBar(context: context);
   }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      if (_counter == 0) {
-        _counter = 1;
-      } else {
-        _counter = 0;
-      }
-    });
-  }
-
+  
   void loadFavorites() {
     Navigator.push(
       context,
