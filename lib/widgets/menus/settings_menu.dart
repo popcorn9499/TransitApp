e91337c9c3a255487889e6f21c508ac2dce3bbfd
popcorn <popcorn9499@gmail.com>
2023-04-26 23:04:49 -0500

    added to settings nearbystop distance and bus schedule times

--- lib/widgets/menus/settings_menu.dart
+++ lib/widgets/menus/settings_menu.dart
@@ -1,9 +1,10 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:transit_app/Config/DarkThemePreference.dart';
 import 'package:transit_app/widgets/menus/main_menu.dart';
 import 'package:transit_app/widgets/widgets/error_snackbar.dart';
 
+import '../../Config/Config.dart';
 import '../widgets/popup_menu.dart';
 
 class SettingsMenu extends StatefulWidget {
@@ -13,13 +14,67 @@ class SettingsMenu extends StatefulWidget {
   SettingsMenuState createState() => SettingsMenuState();
 }
 
+enum Times {
+  one,
+  two,
+  three,
+  four,
+  six,
+  eight,
+  twelve,
+  sixteen,
+  twentyFour;
+
+
+  String valueOf() {
+    String result = "N/A";
+    switch(index) {
+      case 0:
+        result = "1";
+        break;
+      case 1:
+        result = "2";
+        break;
+      case 2:
+        result = "3";
+        break;
+      case 3:
+        result = "4";
+        break;
+      case 4:
+        result = "6";
+        break;
+      case 5:
+        result = "8";
+        break;
+      case 6:
+        result = "12";
+        break;
+      case 7:
+        result = "16";
+        break;
+      case 8:
+        result = "24";
+        break;
+    }
+    return result;
+  }
+}
+
+
 class SettingsMenuState extends State<SettingsMenu> {
   late ErrorSnackBar errorPrompt;
   bool darkMode = false;
+  double nearbyStopsDist = 0.5;
+  int busScheduleMaxResultTime = 2;
+  bool use24Hour = false;
+  ValueNotifier<Times> _selectedItem = new ValueNotifier<Times>(Times.two);
+
   @override
   initState() {
-      WidgetsBinding.instance
-        .addPostFrameCallback((_) => loadSettings()); //run a start item on startup
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) =>
+        loadSettings()); //run a start item on startup
     bool darkValue = false;
 
 
@@ -30,47 +85,110 @@ class SettingsMenuState extends State<SettingsMenu> {
 
   Future<void> loadSettings() async {
     bool darkValue = await DarkThemePreference().getTheme();
+    double nearbyStopsDistance = await Config().getNearbyStopDist();
+    int maxScheduleTime = await Config().getBusScheduleMaxResultTime();
+    bool using24Hourtime = await Config().getUse24HourTimes();
+
     setState(() {
       darkMode = darkValue;
+      nearbyStopsDist = nearbyStopsDistance;
+      busScheduleMaxResultTime = maxScheduleTime;
+      use24Hour = using24Hourtime;
     });
   }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Settings"),
-        ),
-        body: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 400),
-            child: ListView(
-              children: [
-                _SingleSection(
-                  title: "General",
-                  children: [
-                    _CustomListTile(
-                        title: "Dark Mode",
-                        icon: Icons.dark_mode_outlined,
-                        trailing: Switch(
-                            value: darkMode,
-                            onChanged: (value) {
-                              DarkThemePreference().setDarkTheme(value);
-                              isLightTheme.add(value ? ThemeMode.dark : ThemeMode.light);
-                              setState(() {
-                                darkMode = value;
-                              });
-                            })),
-                    const _CustomListTile(
-                        title: "Notifications",
-                        icon: Icons.notifications_none_rounded),
-                  ],
-                )
-              ],
-            ),
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 400),
+          child: ListView(
+            children: [
+              _SingleSection(
+                title: "General",
+                children: [
+                  _CustomListTile(
+                      title: "Dark Mode",
+                      icon: Icons.dark_mode_outlined,
+                      trailing: Switch(
+                          value: darkMode,
+                          onChanged: (value) {
+                            DarkThemePreference().setDarkTheme(value);
+                            isLightTheme.add(
+                                value ? ThemeMode.dark : ThemeMode.light);
+                            setState(() {
+                              darkMode = value;
+                            });
+                          })),
+                  const _CustomListTile(
+                      title: "Notifications",
+                      icon: Icons.notifications_none_rounded),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nearby stops distance ${nearbyStopsDist}km",
+                        border: const OutlineInputBorder()),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    onChanged: (String value) {
+                      setState() {
+                        nearbyStopsDist = double.parse(value);
+                        Config().setNearbyStopDist(nearbyStopsDist);
+                      }
+                    },
+                    onSubmitted: (String value) {
+                      double doub = double.parse(value);
+                      Config().setNearbyStopDist(doub);
+                      setState() {
+                        nearbyStopsDist = doub;
+                      }
+                    },
+                  ),
+                  PopupMenuButton<Times>(
+                    itemBuilder: (BuildContext context) {
+                      return List<PopupMenuEntry<Times>>.generate(
+                        Times.values.length,
+                            (int index) {
+                          return PopupMenuItem(
+                            value: Times.values[index],
+                            child: AnimatedBuilder(
+
+                              animation: _selectedItem,
+                              builder: (BuildContext context, Widget? child) {
+                                return RadioListTile<Times>(
+                                  value: Times.values[index],
+                                  groupValue: _selectedItem.value,
+                                  title: child,
+                                  onChanged: (Times? value) {
+                                    if (value != null) {
+                                      _selectedItem.value = value;
+                                    }
+                                  },
+                                );
+
+                              },
+                              child: Text("${Times.values[index].valueOf()} Hours"),
+                            ),
+
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ],
+              )
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
 
@@ -78,6 +196,7 @@ class _CustomListTile extends StatelessWidget {
   final String title;
   final IconData icon;
   final Widget? trailing;
+
   const _CustomListTile(
       {Key? key, required this.title, required this.icon, this.trailing})
       : super(key: key);
@@ -96,6 +215,7 @@ class _CustomListTile extends StatelessWidget {
 class _SingleSection extends StatelessWidget {
   final String? title;
   final List<Widget> children;
+
   const _SingleSection({
     Key? key,
     this.title,
