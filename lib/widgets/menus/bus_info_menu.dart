4ebfd0fd7898c994bc2526aa1a67da80c2dc6652
popcorn9499 <popcorn9499@gmail.com>
2025-07-09 00:07:01 -0500

    add features for a new bus info menu header

--- lib/widgets/menus/bus_info_menu.dart
+++ lib/widgets/menus/bus_info_menu.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:transit_app/widgets/widgets/bus_info_header.dart';
 import 'package:transit_app/widgets/widgets/error_snackbar.dart';
 
 import '../../api/DataModels/bus_info.dart';
+import '../../bus_status.dart';
+import '../../hex_color.dart';
 import '../widgets/bus_stop_list_tile.dart';
 import '../widgets/popup_menu.dart';
 
 class BusInfoMenu extends StatefulWidget {
   final BusInfo busInfo;
+
   const BusInfoMenu({super.key, required this.busInfo});
 
   @override
@@ -15,9 +19,14 @@ class BusInfoMenu extends StatefulWidget {
 
 class BusInfoMenuState extends State<BusInfoMenu> {
   var newList = <BusStopListTile>[];
-  String routeName = "Example";
-  DateTime lookupTime = DateTime.now();
   late ErrorSnackBar errorPrompt;
+
+  late String timeRemaining;
+  late BusStatus busStatus;
+  late String stopName;
+  late String busNumber;
+  late Color busColor;
+  final double badgeColorInterpretation = 0.95;
 
   BusInfoMenuState();
 
@@ -25,6 +34,11 @@ class BusInfoMenuState extends State<BusInfoMenu> {
   @override
   initState() {
     super.initState();
+    timeRemaining = "0 minutes";
+    busStatus = widget.busInfo.getOnTime();
+    stopName = widget.busInfo.getName();
+    busNumber = widget.busInfo.route.number;
+    busColor = HexColor(widget.busInfo.route.borderColor);
     errorPrompt = ErrorSnackBar(context: context);
     // WidgetsBinding.instance
     //     .addPostFrameCallback((_) => loadFavoritesList()); //run a start item on startup
@@ -33,6 +47,18 @@ class BusInfoMenuState extends State<BusInfoMenu> {
 
   @override
   Widget build(BuildContext context) {
+    String busNumber = this.busNumber;
+    if (this.busNumber.length >= 3) {
+      busNumber = busNumber.substring(0,3);
+    }
+
+    //adjust the color to make the bus badge colors slightly nicer for viewing
+    HSVColor text = HSVColor.fromColor(Theme.of(context).secondaryHeaderColor);
+    HSVColor busCol = HSVColor.fromColor(busColor);
+    HSVColor? col = HSVColor.lerp(text, busCol, badgeColorInterpretation);
+    col ??= HSVColor.fromColor(busColor);
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Bus Information"),
@@ -49,6 +75,36 @@ class BusInfoMenuState extends State<BusInfoMenu> {
             });
           },
           child: Column(children: <Widget>[
+            BusInfoHeader(routeName: widget.busInfo.stop.name, time: DateTime.now(), busInfo: widget.busInfo),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 45, //this should not be static
+                  margin: const EdgeInsets.all(2.0),
+                  padding: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: col.toColor()),
+                    color: col.toColor(),
+                  ),
+                  child: Center(child: RichText(text: TextSpan(text: busNumber, style: TextStyle(
+                    color: text.toColor(),
+                  )))),
+                ),
+                Expanded(
+                  flex: 6,
+                  child: Text(stopName, textAlign: TextAlign.left),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(busStatus.toShortString(), textAlign: TextAlign.right),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(timeRemaining, textAlign: TextAlign.right),
+                ),
+              ],
+          ),
             Expanded(
               child: ListView.builder(
                 itemCount: newList.length,
