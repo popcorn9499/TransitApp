fd6aae66434f9c482d5c4d58e88f9381de7d9544
popcorn9499 <popcorn9499@gmail.com>
2025-08-02 20:01:19 -0500

    fixed naming of bus status enums so dart analysis gods are happy, while retaining the upper case I wanted.

--- lib/bus_status.dart
+++ lib/bus_status.dart
@@ -1,12 +1,11 @@
 
 
-enum BusStatus { Ok, Late, Early, Cancelled }
+enum BusStatus { ok, late, early, cancelled }
 
 //meant to convert the enum to a string
 extension ParseToString on BusStatus {
   String toShortString() {
-    return toString()
-        .split('.')
-        .last;
+    final raw = toString().split('.').last;
+    return raw[0].toUpperCase() + raw.substring(1);
   }
 }
