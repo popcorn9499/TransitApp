cbd4bc7c1cd779024c421ae15c3a98c8e635b4e0
RenegadeRaven <8683689+RenegadeRaven@users.noreply.github.com>
2025-08-03 00:51:18 -0500

    Show arrival time over 60mins as 12/24-hour time Not tested as there are no busses at this hour

--- lib/widgets/widgets/bus_list_tile.dart
+++ lib/widgets/widgets/bus_list_tile.dart
@@ -3,27 +3,61 @@ import "package:transit_app/bus_status.dart";
 import 'package:transit_app/hex_color.dart';
 import 'package:transit_app/widgets/menus/bus_info_menu.dart';
 
+import '../../Config/config.dart';
 import '../../api/DataModels/bus_info.dart';
 
-class BusListTile extends StatelessWidget {
-  BusListTile(this.busInfo, DateTime lookupTime,
-      {super.key}) {
-    int remaining = busInfo.arrivalEstimated
-        .difference(lookupTime)
-        .inMinutes;
-    timeRemaining = (remaining > 60 ? "${busInfo.arrivalEstimated.hour}:${busInfo.arrivalEstimated.minute.toString().padLeft(2, '0')}" : "$remaining min");
-    busStatus = busInfo.getOnTime();
-    stopName = busInfo.getName();
-    busNumber = busInfo.route.number;
-    busColor = HexColor(busInfo.route.borderColor);
-  }
+class BusListTile extends StatefulWidget {
+  final dynamic busInfo;
+  final DateTime lookupTime;
 
-  BusInfo busInfo;
-  late String timeRemaining;
+  const BusListTile(this.busInfo, this.lookupTime, {super.key});
+
+
+  @override
+  BusListTileState createState() => BusListTileState();
+}
+
+class BusListTileState extends State<BusListTile> {
+  int remaining = 0;
+  bool use24Hour = false;
+  String timeRemaining = "";
   late BusStatus busStatus;
+
   late String stopName;
   late String busNumber;
-  late Color busColor;
+  late HexColor busColor;
+
+  BusListTileState();
+
+  @override
+  initState() {
+    super.initState();
+    remaining = widget.busInfo.arrivalEstimated
+          .difference(widget.lookupTime)
+          .inMinutes;
+    WidgetsBinding.instance
+          .addPostFrameCallback((_) => load24HourClock());
+    if (remaining <= 60) {
+      timeRemaining = "$remaining min";
+    } else if (use24Hour) {
+      timeRemaining = "${widget.busInfo.arrivalEstimated.hour}:${widget.busInfo.arrivalEstimated.minute.toString().padLeft(2, '0')}";
+    } else {
+      timeRemaining = "${(widget.busInfo.arrivalEstimated.hour % 12).toString().padLeft(2, '0').replaceAll("00", "12")}:${widget.busInfo.arrivalEstimated.minute.toString().padLeft(2, '0')} ${["AM", "PM"][widget.busInfo.arrivalEstimated.hour >= 12]}";
+    }
+    busStatus = widget.busInfo.getOnTime();
+    stopName = widget.busInfo.getName();
+    busNumber = widget.busInfo.route.number;
+    busColor = HexColor(widget.busInfo.route.borderColor);
+  }
+
+  Future<void> load24HourClock() async {
+    bool use24Hour = await Config().getUse24HourTimes();
+
+    setState(() {
+      this.use24Hour = use24Hour;
+    });
+  }
+
   final double badgeColorInterpretation = 0.95;
 
 
@@ -43,7 +77,7 @@ class BusListTile extends StatelessWidget {
     return ListTile(
       onTap: () { Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => BusInfoMenu(busInfo: busInfo)),
+        MaterialPageRoute(builder: (context) => BusInfoMenu(busInfo: widget.busInfo)),
       );},
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -66,7 +100,7 @@ class BusListTile extends StatelessWidget {
           ),
           Expanded(
             flex: 2,
-            child: Text(busStatus.toShortString(), textAlign: TextAlign.center, style: TextStyle(fontSize: 15)),
+            child: Text(busStatus.toShortString(), textAlign: TextAlign.right, style: TextStyle(fontSize: 15)),
           ),
           Expanded(
             flex: 2,
